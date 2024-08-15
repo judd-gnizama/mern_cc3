78e459d82c3ea149732481e706a194765433b501
judd.gnizama <juddmaverick@gmail.com>
2024-08-16 01:08:46 +0800

    1-judd-added test component and added global context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { useGlobalContext } from "./contexts/GlobalContextProvider";
+
 const App = () => {
-  return <h1>Hellow</h1>;
+  // test fetch
+  const [message, setMessage] = useState("");
+  const { backendURL } = useGlobalContext();
+
+  const fetchMessage = async () => {
+    try {
+      const res = await fetch(`${backendURL}/api/test`);
+      const data = await res.json();
+      setMessage(data.message);
+    } catch (error) {
+      setMessage(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMessage();
+  }, []);
+
+  return (
+    <>
+      <h1>Hello!</h1>
+      <p>Server says:</p>
+      <p>{message}</p>
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/contexts/GlobalContextProvider.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+// const BACKEND_URL = "https://mern-cc3.vercel.app";
+const BACKEND_URL = "http://localhost:5000";
+
+const globalContext = createContext({
+  backendURL: "",
+});
+
+const GlobalContextProvider = ({ children }) => {
+  const [backendURL, setBackendURL] = useState(BACKEND_URL);
+
+  return (
+    <globalContext.Provider value={{ backendURL }}>
+      {children}
+    </globalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(globalContext);
+export default GlobalContextProvider;
